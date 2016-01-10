9a87e8b51de762afc415e3450f98da8162fd693e
jhwatts <jhwatts2010@gmail.com>
2016-01-10 00:18:20 -0500

    Create gspec.R

---
+++ gspec.R
@@ -0,0 +1,28 @@
+#INEL Gamma Spectroscopy data viewer
+
+library(shiny)
+
+row <- function(...) {
+  tags$div(class="row", ...)
+}
+
+col <- function(width, ...) {
+  tags$div(class=paste0("span", width), ...)
+}
+
+server <- function(input, output) {
+}
+
+ui <- shinyUI(bootstrapPage(
+  headerPanel("PDF VIEWER"),
+  mainPanel(
+    tags$div(
+      class = "container",
+      row(
+      col(2, tags$iframe(style="height:400px; width:50%",src="http://www4vip.inl.gov/gammaray/catalogs/nai/pdf/u-ore.pdf"))
+      )
+    )
+  )
+))
+
+shinyApp(ui = ui, server = server)
