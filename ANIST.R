2484a43c6b082c6eb48c38ff5d7f3568e1023abe
jhwatts <jhwatts2010@gmail.com>
2016-01-23 21:36:12 -0500

    Update ANIST.R

--- ANIST.R
+++ ANIST.R
@@ -13,16 +13,3 @@ d <- PlainTextDocument(d, heading = "Plain text document",
 d <- data.frame(d[1])
 
 
-
-library(XML) 
-d <- read_file(file=file.choose())
-
-# download html
-#html <- getURL("http://physics.nist.gov/PhysRefData/XrayMassCoef/ElemTab/z02.html", followlocation = TRUE) 
-# parse html
-
-doc = htmlParse(d, asText=TRUE)
-
-plain.text <- xpathSApply(doc, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
-
-plain.text
